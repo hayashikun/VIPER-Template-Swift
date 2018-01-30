5966b1a9b00e5a6274b29101ff5950d2b4804022
Ryosuke Hayashi <hayashi@hayashikun.com>
2018-01-30 23:33:51 +0900

    Fix typo

--- Presenter.xctemplate/___FILEBASENAME___Presenter.swift
+++ Presenter.xctemplate/___FILEBASENAME___Presenter.swift
@@ -11,6 +11,6 @@ import Foundation
 class ___VARIABLE_Module___Presenter: ___VARIABLE_Module___Presentation, ___VARIABLE_Module___InteractorOutput {
     weak var view: ___VARIABLE_Module___View?
     var interactor: ___VARIABLE_Module___Interaction!
-    var router: ___VARIABLE_Module___Route!Interactor
+    var router: ___VARIABLE_Module___Route!
 }
 
